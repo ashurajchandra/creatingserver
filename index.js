87442d10a127a64bd5af51a69b45f8c755c0d4c5
ASHU <ashutosh>
2020-07-07 19:41:11 +0530

    created my first server

---
+++ index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const port = 8000;
+const fs = require('fs');
+
+
+function requestHandler(req, res){
+    console.log(req.url);
+    res.writeHead(200, {'conten-type':'text/html'});
+  //  res.end("<h1>my nam is lakhan</h1>");
+  let filePath;
+   
+  switch(req.url){
+      case '/':
+          filePath = './index.html'
+          break;
+            case '/profile':
+                filePath = './profile.html'
+                  break;
+                  default:
+                      filePath = "./404.html"
+  }
+fs.readFile(filePath, function(err, data){
+ if(err){
+     console.log('error', err);
+     return res.end('<h1>Error!</h1>')
+ }
+ return res.end(data);
+});
+
+}
+
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, function(err){
+if(err){
+    console.log(err);
+}
+console.log("server is running on port", port);
+
+});
